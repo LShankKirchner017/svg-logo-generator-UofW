465b8d8871e397ac2720eb3230e97d02b650efdb
Lindsay <lshank017@gmail.com>
2023-08-14 18:19:02 -0500

    fixed the module import error for Jest, made Shape test

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,12 +1,19 @@
-// Import shapes 
-import { Circle, Square, Triangle} from "./lib/shapes.js"
+import { Shape }from "../lib/shapes.js";
 
-// Circle Shape
-describe('Circle', () => {
-    test('renders correctly', () => {
-      const shape = new Circle();
-      var color =('blue')
-      shape.setColor(color);
-      expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${color}">`);
-    });
-  });
+// Shape 
+describe('Shape', () => {
+  it('renders with correct color', () => {
+    var color = 'red'
+    const shape = new Shape(color)
+    expect (shape.color).toBe(color)
+    
+  })
+  it('throw an error if call render on Shape', () => {
+    var color = "red";
+    const shape = new Shape(color);
+    const foo = () => shape.render()
+    expect(foo).toThrow(Error("Child class must use render() method"));
+  })
+})
+
+// Circle
